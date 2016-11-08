421243488bf14506ea93fd0dc3834bb481e94561
vduron <vivianaduron@gmail.com>
2016-11-08 15:15:27 -0800

    Viviana Portfolio - Final Project (Update 5)
    Final Project Update - Project page is running each project's images but still won't pick up the descriptions.

---
+++ vivianaPortfolioUpdate/js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+   animateScroll();
+});
+
+function animateScroll()
+    {
+    $('#psdBar').animate({
+    width: '100%'
+}, {
+    duration: 3000,
+    easing: 'linear',
+    complete:function(){ $(this).css("width","0%");
+   }
+});
+animateScroll();
+}
